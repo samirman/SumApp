81303351e12bd8fab5678761f50a3c80ec1abf48
James Conallen <jconallen@us.ibm.com>
2018-01-08 14:59:07 -0500

    move to default namespace

--- deployment/podTemplate.groovy
+++ deployment/podTemplate.groovy
@@ -1,7 +1,7 @@
 podTemplate(label: 'docker', namespace: 'default',
   containers: [
     containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat'),
-    containerTemplate(name: 'docker', image: 'docker:1.11', ttyEnabled: true, command: 'cat'),
+    containerTemplate(name: 'docker', image: 'docker:stable', ttyEnabled: true, command: 'cat'),
     containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.8.0', command: 'cat', ttyEnabled: true)
   ],
   volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]
@@ -9,11 +9,11 @@ podTemplate(label: 'docker', namespace: 'default',
 
   def gitSrc = 'https://github.com/jconallen/SumApp.git'
   def dockerRegistry = 'https://mycluster.icp:8500'
-  def image = 'mycluster.icp:8500/demo/sumapp'
+  def image = 'mycluster.icp:8500/default/sumapp'
   def deployment = 'deployment/sumapp-deploy.yml'
   def service = 'deployment/sumapp-svc.yml'
   def selector = 'sumapp'
-  def namespace = 'demo'
+  def namespace = 'default'
   
   node('docker') {
     
@@ -26,9 +26,9 @@ podTemplate(label: 'docker', namespace: 'default',
           sh "mvn -B clean package"
       }
     }
-    docker.withRegistry("${dockerRegistry}", 'icp-id') {
-      stage('Build Docker image') {
-        container('docker') {
+    stage('Build Docker image') {
+      container('docker') {
+        docker.withRegistry("${dockerRegistry}", 'icp-id') {
           def props = readProperties  file:'deployment/pipeline.properties'
           def tag = props['version']
           sh "docker build -t ${image}:${tag} ."
@@ -46,6 +46,7 @@ podTemplate(label: 'docker', namespace: 'default',
                             passwordVariable: 'DOCKER_HUB_PASSWORD']]) {
             
             sh "kubectl delete deployments -n ${namespace} --selector=app=${selector}"
+            sh "kubectl delete services -n ${namespace} --selector=app=${selector}"
         }
       } 
     }
